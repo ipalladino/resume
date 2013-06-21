cf085758724a561433dde416138234958b94cec9
Ignacio Palladino <ignacio@flingo.tv>
2013-06-21 15:21:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resume::Application.config.secret_token = '93a7121b41232e7b38bf2d7e81faae2145e5456d60de8955613f1b404bc630c045890971036c73f89bd98d94211a60722fb40aa06fac8169048374308f891200'
